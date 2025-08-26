54aae12c1b9bedf13b6ed1c30f59a73aaf1df2d2
iamfab <fabian2015hd@gmail.com>
2025-08-26 11:38:22 -0400

    url deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
   //Backend data
   production: false,
   BACK_END_HOST_PROD: 'https://devpws.cs.umss.edu.bo/api/v1',
-  BACK_END_HOST_DEV: 'http://localhost:9090/api/v1',
-  BACK_END_HOST_DEV_AUTH: 'http://localhost:9090/api/auth',
+  BACK_END_HOST_DEV: 'http://dpa.umss.net/api/v1',
+  BACK_END_HOST_DEV_AUTH: 'http://dpa.umss.net/api/auth',
 
   //Facebook data
   GRAPH_FACEBOOK_API_URL: 'https://graph.facebook.com',
