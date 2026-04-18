f73dc8a294fa14912ec92625e13be87b40f58956
Omar Selaya <201900837@est.umss.edu>
2026-04-17 23:17:12 -0400

    Fix delete section within itself

--- src/app/components/navbar/form-section/form-section.component.ts
+++ src/app/components/navbar/form-section/form-section.component.ts
@@ -6,6 +6,8 @@ import { MessageService } from 'primeng/api';
 import moment from 'moment';
 import { SectionStateService } from '../../../pages/services/sections-state.service';
 import { environment } from '../../../../environments/environment';
+import { ActivatedRoute, Router } from '@angular/router';
+import { concatMap, finalize, map, Observable, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-form-section',
@@ -17,6 +19,8 @@ export class FormSectionComponent implements OnInit, OnChanges {
   private readonly messageService = inject(MessageService);
   private readonly sectionStateService = inject(SectionStateService);
   private readonly institutionId = environment.INSTITUTION_ID;
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
 
   @Input() typeForm: 'create' | 'edit' = 'create';
   @Input() currentSection: Section | undefined;
@@ -111,21 +115,58 @@ export class FormSectionComponent implements OnInit, OnChanges {
     if(!this.currentSection) return;
 
     this.isLoading = true;
-    this.informationService.deleteSection(this.currentSection.uuid).subscribe({
-      next: () => {
-        this.isLoading = false;
-        this.messageService.add({ severity: 'success', summary: 'Exitoso', detail: 'Sección eliminada exitosamente' });
-        this.onDeletedSection.emit(this.currentSection);
-        this.onCloseEdit.emit();
-        this.currentSection = undefined;
-      },
-      error: (err) => {
-        this.isLoading = false;
-        console.log('error',err)
-        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al eliminar sección' });
-        this.onCloseEdit.emit();
+    
+    this.informationService.deleteSection(this.currentSection.uuid).pipe(
+      tap({
+        next: () => this.handleDeleteSuccess(),
+        error: (err) => this.handleDeleteError(err)
+      }),
+      concatMap(() => this.handleRedirectionIfNeeded()),
+      finalize(() => this.finalizeDelete())
+    ).subscribe();
+  }
+
+  private handleDeleteSuccess(): void {
+    this.messageService.add({ severity: 'success', summary: 'Exitoso', detail: 'Sección eliminada exitosamente' });
+    this.onDeletedSection.emit(this.currentSection);
+    this.onCloseEdit.emit();
+  }
+
+  private handleDeleteError(err: any): void {
+    console.log('error', err);
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al eliminar sección' });
+    this.onCloseEdit.emit();
+  }
+
+  private handleRedirectionIfNeeded(): Observable<void> {
+    const currentSectionUrlUuid = this.route.snapshot.firstChild?.paramMap.get('uuidSection');
+    
+    if (currentSectionUrlUuid && currentSectionUrlUuid === this.currentSection?.uuid) {
+      const currentNavItemUuid = this.route.snapshot.paramMap.get('uuidNavItem');
+      
+      if (currentNavItemUuid) {
+        return this.informationService.getAllSectionsByNavItemId(currentNavItemUuid).pipe(
+          tap(secs => this.performRedirection(currentNavItemUuid, secs)),
+          map(() => void 0)
+        );
       }
-    });
+    }
+    
+    return of(void 0);
+  }
+
+  private performRedirection(currentNavItemUuid: string, sections: any[]): void {
+    if (sections.length > 0) {
+      const firstSection = sections[0];
+      this.router.navigate(['../' + currentNavItemUuid, firstSection.uuid], { relativeTo: this.route });
+    } else {
+      this.router.navigate(['../' + currentNavItemUuid], { relativeTo: this.route });
+    }
+  }
+
+  private finalizeDelete(): void {
+    this.isLoading = false;
+    this.currentSection = undefined;
   }
 
   closeForm(): void {
