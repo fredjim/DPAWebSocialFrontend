374010ce6b3cdf4ae8d67d8ce5e936470118855f
Omar Selaya <201900837@est.umss.edu>
2026-08-03 22:42:35 -0400

    Fix: reactions post mobile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,8 +17,8 @@ export const environment = {
   production: false,
   BACK_END_HOST_PROD: 'https://devpws.cs.umss.edu.bo/api/v1',
   /*BACK_END_HOST_DEV: 'http://dpa.umss.net/api/v1',*/
-  BACK_END_HOST_DEV: 'http://localhost:9091/api/v1',
-  BACK_END_HOST_DEV_AUTH: 'http://localhost:9091/api/auth',
+  BACK_END_HOST_DEV: 'http://192.168.1.43:9091/api/v1',
+  BACK_END_HOST_DEV_AUTH: 'http://192.168.1.43:9091/api/auth',
 
 };
 /*
